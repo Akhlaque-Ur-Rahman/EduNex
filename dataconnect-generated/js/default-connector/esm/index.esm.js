bbdde633578cdd6a585e142dc299fe0b63503431
Akhlaque <ikhlakhrehman@gmail.com>
2025-03-15 23:37:51 +0530

    its live

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'next-app',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,8 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'next-app',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
