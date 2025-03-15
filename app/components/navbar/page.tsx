39474b3a6607e8f3eb61d140a8ed97f98fcf38c6
Akhlaque <ikhlakhrehman@gmail.com>
2025-03-16 00:08:18 +0530

    testing 1

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className="w-full bg-white shadow-sm p-4">
       <div className="flex items-center justify-between">
-        <div className="text-xl font-bold w-1/4">EduNex</div>
+        <div className="text-xl font-bold w-1/4">EduNex 2.0</div>
 
         {/* Mobile menu button */}
         <button 
